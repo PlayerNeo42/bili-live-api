3826751b73b0f8c9aabf3c8c69e9616eb7457001
weiyepeng <weiyepeng@tencent.com>
2022-09-17 18:53:32 +0800

    feat:优化客户端

--- resource/resource.go
+++ resource/resource.go
@@ -8,12 +8,14 @@ const (
 	// LiveAPIURL B站直播API地址
 	LiveAPIURL = "https://api.live.bilibili.com"
 	// APIURL B站API地址
-	APIURL   = "https://api.bilibili.com"
-	VcAPIURL = "https://api.vc.bilibili.com"
+	APIURL         = "https://api.bilibili.com"
+	VcAPIURL       = "https://api.vc.bilibili.com"
+	UserAgentKey   = "User-Agent"
+	UserAgentValue = "curl 7.68.0"
 )
 
 var (
-	liveAPIClient = resty.New().SetBaseURL(LiveAPIURL)
-	apiClient     = resty.New().SetBaseURL(APIURL)
-	vcApiClient   = resty.New().SetBaseURL(VcAPIURL)
+	liveAPIClient = resty.New().SetHeader(UserAgentKey, UserAgentValue).SetBaseURL(LiveAPIURL)
+	apiClient     = resty.New().SetHeader(UserAgentKey, UserAgentValue).SetBaseURL(APIURL)
+	vcApiClient   = resty.New().SetHeader(UserAgentKey, UserAgentValue).SetBaseURL(VcAPIURL)
 )
--- resource/user_test.go
+++ resource/user_test.go
@@ -7,12 +7,13 @@ import (
 
 func TestUserInfo(t *testing.T) {
 	userInfo, err := UserInfo(2075179777)
+	userJson, _ := json.Marshal(userInfo)
+	println(string(userJson))
 	if err != nil {
 		t.Error(err)
 	}
 	if userInfo.Message == "" {
 		t.Errorf("获取用户信息失败, %+v", userInfo)
 	}
-	userJson, _ := json.Marshal(userInfo)
-	println(string(userJson))
+
 }
